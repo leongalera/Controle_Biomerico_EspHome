27e831a4cab6ee8b9a384bdc8c41d16a263a9bb3
leongalera <leongalera@gmail.com>
2025-10-17 17:56:26 -0300

    ProxyFix v2

--- controle_biometrico/serve.py
+++ controle_biometrico/serve.py
@@ -2,6 +2,7 @@
 from run import app  # Importa a variável 'app' do nosso arquivo run.py
 from waitress import serve
 import logging
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
